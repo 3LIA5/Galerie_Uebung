35b65ee34b3dbb0c32bc4654c73a91d2e5af7cd0
Gross3LIA5 <gro19823@spengergasse.at>
2020-06-15 17:22:14 +0200

    FX01_Galerie_1_MenüStruktur.pdf

---
+++ src/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			Scene scene = new Scene(new RootBorderPane(), 650, 300);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Galerie \"FX Arts\" Kunstwerke-Verwaltung");
+			primaryStage.show();		
+		}
+		catch (Exception e)
+		{
+			new Alert(AlertType.ERROR, "Schwerer Fehler beim Start der Applikation!!!\nGrund: "+e.getMessage(), ButtonType.OK).showAndWait();
+			Platform.exit();
+		}
+	}
+	public static void showAlert(AlertType alertType, String message) // boolean RW, damit selbe Methode auch für Rückfragen verwendet werden kann
+	{
+		Alert alert = new Alert(alertType, message);//, ButtonType.OK);
+		alert.setHeaderText(null);
+		alert.setTitle("Achtung!");
+		alert.showAndWait();
+	}
+
+	//-----------------------------------------------------------------------
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
---
+++ src/application/RootBorderPane.java
@@ -0,0 +1,71 @@
+package application;
+
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+public class RootBorderPane extends BorderPane
+{
+	private MenuBar menuBar;
+	private Menu mDatei, mBearbeiten, mHilfe,
+				 mLaden, mSpeichern,  mHinzufuegen, mSortieren;
+	private MenuItem miLadenSerialisiert, miSpeichernSerialisiert, miImportCsv, miExportCsv, miImportFormatiert, miExportFormatiert, miBeenden,
+					 miSortierenKuestler, miSortierenWert, miLoeschen, miAendern, miHinzufuegenBild, miHinzufuegenSkulptur,
+					 miInfo;
+	
+	public RootBorderPane()
+	{
+		initMenues();
+		addMenues();
+		disableCompenents(true);
+	}
+	private void initMenues()
+	{
+		mDatei = new Menu("Datei");
+		mBearbeiten = new Menu("Bearbeiten");
+		mHilfe = new Menu("Hilfe");		
+		mLaden = new Menu("Laden");
+		mSpeichern = new Menu("Speichern");
+		mHinzufuegen = new Menu("Hinzufügen");
+		mSortieren = new Menu("Sortieren");
+		
+		miLadenSerialisiert = new MenuItem("Laden .ser-Datei");
+		miSpeichernSerialisiert = new MenuItem("Speichern .ser-Datei");
+		miExportCsv = new MenuItem("Speichern .csv-Datei");
+		miImportCsv = new MenuItem("Laden .csv-Datei");
+		miExportCsv = new MenuItem("Speichern .csv-Datei");
+		miImportFormatiert = new MenuItem("Laden .txt-Datei formatiert");
+		miExportFormatiert = new MenuItem("Speichern .txt-Datei formatiert");
+		miBeenden = new MenuItem("Beenden");
+//			miBeenden.setStyle("-fx-background-color: limeGreen; -fx-text-fill: RED");
+//			miBeenden.setStyle("-fx-background-color: AQUA; -fx-text-fill: RED"); // vor-definierte Farben in der Klasse Color
+		miHinzufuegenBild = new MenuItem("Bild");
+		miHinzufuegenSkulptur = new MenuItem("Skulptur");
+		
+		miSortierenKuestler = new MenuItem("aufsteigend nach Künstler");
+		miSortierenWert = new MenuItem("absteigend nach Wert");
+		miLoeschen = new MenuItem("Löschen");
+		miAendern = new MenuItem("Ändern");
+		
+		miInfo = new MenuItem("Über");
+	}
+	private void addMenues()
+	{
+		mDatei.getItems().addAll(mLaden, mSpeichern, miBeenden);
+		mLaden.getItems().addAll(miLadenSerialisiert, miImportCsv, miImportFormatiert);
+		mSpeichern.getItems().addAll(miSpeichernSerialisiert, miExportCsv, miExportFormatiert);
+		mBearbeiten.getItems().addAll(mSortieren, miLoeschen, miAendern, mHinzufuegen);
+		mSortieren.getItems().addAll(miSortierenKuestler, miSortierenWert);
+		mHinzufuegen.getItems().addAll(miHinzufuegenBild, miHinzufuegenSkulptur);
+		mHilfe.getItems().add(miInfo);
+		
+		menuBar = new MenuBar(mDatei, mBearbeiten, mHilfe);		
+		setTop(menuBar);
+	}
+	private void disableCompenents(boolean disable)
+	{
+		mSpeichern.setDisable(disable);
+		mSortieren.setDisable(disable);
+		miLoeschen.setDisable(disable);
+		miAendern.setDisable(disable);
+	}
+}
